857b7d45d5319e9b1396ad0ebdb543007104688a
TJ Saunders <tj@castaglia.org>
2012-03-10 17:33:01 -0800

    Fleshing out more of the necessary mod_tar regression tests.  We're now ready to perform open-heart surgery on the mod_tar code, and replace its use of the libtar library with the (hopefully better) libarchive library.

--- t/lib/ProFTPD/Tests/Modules/mod_tar.pm
+++ t/lib/ProFTPD/Tests/Modules/mod_tar.pm
@@ -74,14 +74,27 @@ my $TESTS = {
 #    test_class => [qw(forking)],
 #  },
 
-  tar_retr_2gb => {
+  tar_xferlog_retr_tar => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
+  tar_tmp_path_cleanup_on_abort => {
+    order => ++$order,
+    test_class => [qw(forking)],
+  },
+
+  tar_retr_tar_2gb => {
     order => ++$order,
     test_class => [qw(forking slow)],
   },
 
-  # tar_temp_path, aborted downloads and cleanup
+  # XXX Need libarchive for this to work
+#  tar_retr_zip => {
+#    order => ++$order,
+#    test_class => [qw(forking)],
+#  },
 
-  # tar_xferlog, making sure the originally requested name is logged
 };
 
 sub new {
@@ -89,10 +102,7 @@ sub new {
 }
 
 sub list_tests {
-#  return testsuite_get_runnable_tests($TESTS);
-  return qw(
-    tar_notar
-  );
+  return testsuite_get_runnable_tests($TESTS);
 }
 
 sub tar_retr_file {
@@ -1694,6 +1704,635 @@ sub tar_retr_tgz {
 
   } else {
     eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub tar_xferlog_retr_tar {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/tar.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/tar.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/tar.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/tar.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/tar.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/subdir");
+  mkpath($sub_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $test_file = File::Spec->rel2abs("$sub_dir/test.txt");
+  if (open(my $fh, "> $test_file")) {
+    print $fh "Hello, World!\n";
+    unless (close($fh)) {
+      die("Can't write $test_file: $!");
+    }
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+
+  # Calculate the MD5 checksum of this file, for comparison with the
+  # downloaded file.
+  my $ctx = Digest::MD5->new();
+  my $expected_md5;
+
+  if (open(my $fh, "< $test_file")) {
+    binmode($fh);
+    $ctx->addfile($fh);
+    $expected_md5 = $ctx->hexdigest();
+    close($fh);
+
+  } else {
+    die("Can't read $test_file: $!");
+  }
+
+  my $xfer_log = File::Spec->rel2abs("$tmpdir/xfer.log");
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    AllowOverwrite => 'on',
+    AllowStoreRestart => 'on',
+
+    TransferLog => $xfer_log,
+
+    IfModules => {
+      'mod_tar.c' => {
+        TarEngine => 'on',
+        TarLog => $log_file,
+      },
+
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $conn = $client->retr_raw("subdir.tar");
+      unless ($conn) {
+        die("RETR subdir.tar failed: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $tar = Archive::Tar->new($conn);
+      eval { $conn->close() };
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected response code $expected, got $resp_code"));
+
+      $expected = 'Transfer complete';
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected response message '$expected', got '$resp_msg'"));
+
+      $client->quit();
+
+      my $entries = { map { $_ => 1 } $tar->list_files() };
+
+      # Make sure the hashref contains the entries we expect
+      $expected = 2;
+      my $nents = scalar(keys(%$entries));
+      $self->assert($nents == $expected,
+        test_msg("Expected $expected entries, found $nents"));
+
+      $expected = 'subdir/';
+      $self->assert(defined($entries->{$expected}),
+        test_msg("Expected entry for '$expected', did not see one"));
+
+      $expected = 'subdir/test.txt';
+      $self->assert(defined($entries->{$expected}),
+        test_msg("Expected entry for '$expected', did not see one"));
+
+      # Make sure the file contents have not been corrupted in transit
+
+      $ctx = Digest::MD5->new();
+      $ctx->add($tar->get_content('subdir/test.txt')); 
+
+      my $test_md5 = $ctx->hexdigest();
+
+      $self->assert($test_md5 eq $expected_md5,
+        test_msg("Expected MD5 checksum '$expected_md5', got '$test_md5'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  eval {
+    # Now read in the TransferLog, make sure that the filename is the originally
+    # requested name, not the name as modified by mod_tar.
+
+    if (open(my $fh, "< $xfer_log")) {
+      my $line = <$fh>;
+      chomp($line);
+      close($fh);
+
+      my $expected = '^\S+\s+\S+\s+\d+\s+\d+:\d+:\d+\s+\d+\s+\d+\s+(\S+)\s+(\d+)\s+(\S+)\s+(\S+)\s+_\s+o\s+r\s+(\S+)\s+ftp\s+0\s+\*\s+c$';
+
+      $self->assert(qr/$expected/, $line,
+        test_msg("Expected '$expected', got '$line'"));
+
+      if ($line =~ /$expected/) {
+        my $remote_host = $1;
+        my $filesz = $2;
+        my $filename = $3;
+        my $xfer_type = $4;
+        my $user_name = $5;
+
+        $expected = '127.0.0.1';
+        $self->assert($expected eq $remote_host,
+          test_msg("Expected '$expected', got '$remote_host'"));
+
+        # The original filename, sans .tar extension
+        $expected = $sub_dir;
+
+        if ($^O eq 'darwin') {
+          # MacOSX-specific hack dealing with their tmp filesystem shenanigans
+          $expected = ('/private' . $expected);
+        }
+
+        $self->assert($expected eq $filename,
+          test_msg("Expected '$expected', got '$filename'"));
+
+        $expected = 'a';
+        $self->assert($expected eq $xfer_type,
+          test_msg("Expected '$expected', got '$xfer_type'"));
+
+        $expected = $user;
+        $self->assert($expected eq $user_name,
+          test_msg("Expected '$expected', got '$user_name'"));
+
+      } else {
+        die("Can't read $xfer_log: $!");
+      }
+    }
+  };
+
+  if ($@) {
+    $ex = $@;
+  }
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub tar_tmp_path_cleanup_on_abort {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/tar.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/tar.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/tar.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/tar.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/tar.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/subdir");
+  mkpath($sub_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $test_file = File::Spec->rel2abs("$sub_dir/test.txt");
+  if (open(my $fh, "> $test_file")) {
+    print $fh "Hello, World!\n";
+    unless (close($fh)) {
+      die("Can't write $test_file: $!");
+    }
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+
+  my $tmp_path = File::Spec->rel2abs("$tmpdir/tarwork");
+  mkpath($tmp_path);
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    AllowOverwrite => 'on',
+    AllowStoreRestart => 'on',
+
+    IfModules => {
+      'mod_tar.c' => {
+        TarEngine => 'on',
+        TarLog => $log_file,
+        TarTempPath => $tmp_path,
+      },
+
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $conn = $client->retr_raw("subdir.tar");
+      unless ($conn) {
+        die("RETR subdir.tar failed: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      # Read in a couple of bytes, then abort the connection.
+      my $buf;
+      $conn->read($buf, 4, 25);
+      $conn->abort();
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected response code $expected, got $resp_code"));
+
+      $expected = 'Abort successful';
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected response message '$expected', got '$resp_msg'"));
+
+      $client->quit();
+
+      # Scan the TarTempPath, make sure there are no files in there.
+      my $dirh;
+      unless (opendir($dirh, $tmp_path)) {
+        die("Can't open directory '$tmp_path': $!");
+      }
+
+      my $tmp_files = [grep { !/^\.$/ && !/^\.\.$/ } readdir($dirh)];
+      closedir($dirh);
+
+      my $nfiles = scalar(@$tmp_files);
+      $self->assert($nfiles == 0,
+        test_msg("Expected no tmp files, found $nfiles"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh) };
+    if ($@) {
+      warn($@);
+      exit 1;
+    }
+
+    exit 0;
+  }
+
+  # Stop server
+  server_stop($pid_file);
+
+  $self->assert_child_ok($pid);
+
+  if ($ex) {
+    test_append_logfile($log_file, $ex);
+    unlink($log_file);
+
+    die($ex);
+  }
+
+  unlink($log_file);
+}
+
+sub tar_retr_tar_2gb {
+  my $self = shift;
+  my $tmpdir = $self->{tmpdir};
+
+  my $config_file = "$tmpdir/tar.conf";
+  my $pid_file = File::Spec->rel2abs("$tmpdir/tar.pid");
+  my $scoreboard_file = File::Spec->rel2abs("$tmpdir/tar.scoreboard");
+
+  my $log_file = test_get_logfile();
+
+  my $auth_user_file = File::Spec->rel2abs("$tmpdir/tar.passwd");
+  my $auth_group_file = File::Spec->rel2abs("$tmpdir/tar.group");
+
+  my $user = 'proftpd';
+  my $passwd = 'test';
+  my $group = 'ftpd';
+  my $home_dir = File::Spec->rel2abs($tmpdir);
+  my $uid = 500;
+  my $gid = 500;
+
+  my $sub_dir = File::Spec->rel2abs("$tmpdir/subdir");
+  mkpath($sub_dir);
+
+  # Make sure that, if we're running as root, that the home directory has
+  # permissions/privs set for the account we create
+  if ($< == 0) {
+    unless (chmod(0755, $home_dir)) {
+      die("Can't set perms on $home_dir to 0755: $!");
+    }
+
+    unless (chown($uid, $gid, $home_dir)) {
+      die("Can't set owner of $home_dir to $uid/$gid: $!");
+    }
+  }
+
+  auth_user_write($auth_user_file, $user, $passwd, $uid, $gid, $home_dir,
+    '/bin/bash');
+  auth_group_write($auth_group_file, $group, $gid, $user);
+
+  my $test_file = File::Spec->rel2abs("$sub_dir/test.txt");
+
+  # Create a file that is 2GB plus 24 bytes.
+  my $test_len = (2 ** 31) + 24;
+  if (open(my $fh, "> $test_file")) {
+
+    if ($ENV{TEST_VERBOSE}) {
+      print STDOUT "# Creating test file of $test_len bytes\n";
+    }
+
+    # Seek to the 2GB limit, then fill the rest with 'A'
+    unless (seek($fh, (2 ** 31), 0)) {
+       die("Can't seek to 2GB length: $!");
+    }
+
+    print $fh "A" x 24;
+
+    unless (close($fh)) {
+      die("Can't write $test_file: $!");
+    }
+
+  } else {
+    die("Can't open $test_file: $!");
+  }
+
+  # Calculate the MD5 checksum of this file, for comparison with the
+  # downloaded file.
+  my $ctx = Digest::MD5->new();
+  my $expected_md5;
+
+  if (open(my $fh, "< $test_file")) {
+    binmode($fh);
+    $ctx->addfile($fh);
+    $expected_md5 = $ctx->hexdigest();
+    close($fh);
+
+  } else {
+    die("Can't read $test_file: $!");
+  }
+
+  # This test could run for a while, since mod_tar has to read in the large
+  # file.  So give the test time to run.
+  my $timeout_idle = 1800;
+
+  my $config = {
+    PidFile => $pid_file,
+    ScoreboardFile => $scoreboard_file,
+    SystemLog => $log_file,
+
+    AuthUserFile => $auth_user_file,
+    AuthGroupFile => $auth_group_file,
+
+    AllowOverwrite => 'on',
+    AllowStoreRestart => 'on',
+
+    TimeoutIdle => $timeout_idle,
+
+    IfModules => {
+      'mod_tar.c' => {
+        TarEngine => 'on',
+        TarLog => $log_file,
+      },
+
+      'mod_delay.c' => {
+        DelayEngine => 'off',
+      },
+    },
+  };
+
+  my ($port, $config_user, $config_group) = config_write($config_file, $config);
+
+  # Open pipes, for use between the parent and child processes.  Specifically,
+  # the child will indicate when it's done with its test by writing a message
+  # to the parent.
+  my ($rfh, $wfh);
+  unless (pipe($rfh, $wfh)) {
+    die("Can't open pipe: $!");
+  }
+
+  my $ex;
+
+  # Fork child
+  $self->handle_sigchld();
+  defined(my $pid = fork()) or die("Can't fork: $!");
+  if ($pid) {
+    eval {
+      my $client = ProFTPD::TestSuite::FTP->new('127.0.0.1', $port);
+      $client->login($user, $passwd);
+
+      my $conn = $client->retr_raw("subdir.tar");
+      unless ($conn) {
+        die("RETR subdir.tar failed: " . $client->response_code() . " " .
+          $client->response_msg());
+      }
+
+      my $tar = Archive::Tar->new($conn);
+      eval { $conn->close() };
+
+      my $resp_code = $client->response_code();
+      my $resp_msg = $client->response_msg();
+
+      my $expected;
+
+      $expected = 226;
+      $self->assert($expected == $resp_code,
+        test_msg("Expected response code $expected, got $resp_code"));
+
+      $expected = 'Transfer complete';
+      $self->assert($expected eq $resp_msg,
+        test_msg("Expected response message '$expected', got '$resp_msg'"));
+
+      $client->quit();
+
+      my $entries = { map { $_ => 1 } $tar->list_files() };
+
+      # Make sure the hashref contains the entries we expect
+      $expected = 2;
+      my $nents = scalar(keys(%$entries));
+      $self->assert($nents == $expected,
+        test_msg("Expected $expected entries, found $nents"));
+
+      $expected = 'subdir/';
+      $self->assert(defined($entries->{$expected}),
+        test_msg("Expected entry for '$expected', did not see one"));
+
+      $expected = 'subdir/test.txt';
+      $self->assert(defined($entries->{$expected}),
+        test_msg("Expected entry for '$expected', did not see one"));
+
+      # Make sure the file contents have not been corrupted in transit
+
+      $ctx = Digest::MD5->new();
+      $ctx->add($tar->get_content('subdir/test.txt')); 
+
+      my $test_md5 = $ctx->hexdigest();
+
+      $self->assert($test_md5 eq $expected_md5,
+        test_msg("Expected MD5 checksum '$expected_md5', got '$test_md5'"));
+    };
+
+    if ($@) {
+      $ex = $@;
+    }
+
+    $wfh->print("done\n");
+    $wfh->flush();
+
+  } else {
+    eval { server_wait($config_file, $rfh, $timeout_idle + 10) };
     if ($@) {
       warn($@);
       exit 1;
